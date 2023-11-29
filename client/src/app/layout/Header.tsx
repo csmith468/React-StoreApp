c1b7deb8f4741a45746983d2c3b6192a712a04e5
csmith468 <chapin2816@gmail.com>
2023-11-29 15:49:32 -0600

    End of section 4

--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -1,5 +1,29 @@
-import { AppBar, Toolbar, Typography } from "@mui/material";
-import SwitchTheme from "../../features/catalog/SwitchTheme";
+import { AppBar, Badge, Box, IconButton, List, ListItem, Switch, Toolbar, Typography } from "@mui/material";
+import { NavLink } from "react-router-dom";
+import { DarkMode, LightMode, ShoppingCart } from "@mui/icons-material";
+
+const midLinks = [
+    {title: 'catalog', path: '/catalog'},
+    {title: 'about', path: '/about'},
+    {title: 'contact', path: '/contact'},
+]
+
+const rightLinks = [
+    {title: 'login', path: '/login'},
+    {title: 'register', path: '/register'},
+]
+
+const navStyles = {
+    color: 'inherit', 
+    typography: 'h6',
+    textDecoration: 'none',
+    '&:hover': {
+        color: 'grey.500'
+    },
+    '&.active': {
+        color: 'text.secondary'
+    }
+}
 
 interface Props {
     darkMode: boolean;
@@ -9,9 +33,52 @@ interface Props {
 export default function Header({darkMode, handleThemeChange}: Props) {
     return (
         <AppBar position='static' sx={{mb:4}}>
-            <Toolbar>
-                <Typography variant='h6'>React Store</Typography>
-                <SwitchTheme darkMode={darkMode} handleThemeChange={handleThemeChange}/>
+            <Toolbar sx={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
+                <Box display='flex' alignItems='center'>
+                    <Typography variant='h6' 
+                        component={NavLink} 
+                        to='/'
+                        sx={navStyles}
+                    >
+                        React Store
+                    </Typography>
+                    <Box display='flex' alignItems='center'>
+                        <LightMode/>
+                        <Switch checked={darkMode} onChange={handleThemeChange}/>
+                        <DarkMode/>
+                    </Box>
+                </Box>
+                <List sx={{display: 'flex'}}>
+                    {midLinks.map(({title, path}) => (
+                        <ListItem 
+                            component={NavLink}
+                            to={path}
+                            key={path}
+                            sx={navStyles}
+                        >{title.toUpperCase()}
+                        </ListItem>
+                    ))}
+                </List>
+
+                <Box display='flex' alignItems='center'>
+                    <IconButton size='large' edge='start' color='inherit' sx={{mr:2}}>
+                        <Badge badgeContent='4' color='secondary'>
+                            <ShoppingCart />
+                        </Badge>
+                    </IconButton>
+
+                    <List sx={{display: 'flex'}}>
+                        {rightLinks.map(({title, path}) => (
+                            <ListItem 
+                                component={NavLink}
+                                to={path}
+                                key={path}
+                                sx={navStyles}
+                            >{title.toUpperCase()}
+                            </ListItem>
+                        ))}
+                    </List>
+                </Box>
             </Toolbar>
         </AppBar>
     )
